12b2063f3cf52a0a59f7cfd752c2d51f46898279
Prateeksha Tripathy <127588744+prateeksha-tripathy@users.noreply.github.com>
2023-09-05 23:15:50 +0530

    added

---
+++ hallo.c
@@ -0,0 +1,10 @@
+#include<stdio.h>
+#include<conio.h>
+int main(){
+    int a;
+    printf("hello c \n");
+    printf("enter a \n");
+    scanf("%d",&a);
+    printf("value is %d\n",a);
+    return 0;
+}
